8f5453cae5cf5113e4a7f04f38b4ec12a67b6bab
Tim Moody <tim@timmoody.com>
2015-04-13 16:35:13 -0400

    change menu

--- root/var/www/html/xs-portal/index.en.php
+++ root/var/www/html/xs-portal/index.en.php
@@ -5,26 +5,26 @@
 <HTML>
 <HEAD>
 
-<TITLE>Internet in a Box</TITLE>
+<TITLE>XSCE School Server</TITLE>
 
 </HEAD>
 <BODY>
-<div id="wrapper">	
-<h1>Internet in a Box</h1>
+<div id="wrapper">
+<h1>XSCE School Server</h1>
 <?php require 'incl/banner.html'; ?>
 
-<div id="main"> 
+<div id="main">
 <br><h2>Interesting Things You Can Do Here:</h2>
 
 <h2>Internet Collection</h2>
 <?php hard_link("/rachel", "RACHEL", "RACHEL is a collection of some of the most important material on the internet, such as the Wikipedia, stored locally where you can reach it easily."); ?>
 
 <h2>More Wikipedias</h2>
-<?php hard_link("/iiab/zim/iframe/wikipedia_en_all_nopic_01_2012", "Wikipedia in English", "The full English Wikipedia","/library/knowledge/modules/wikipedia-zim/wikipedia_en_all_nopic_01_2012.zim"); ?>
-<?php hard_link("/iiab/zim/iframe/wikipedia_fr_all_09_2012", "Wikipédia en français", "Wikipédia est une encyclopédie multilingue, universelle et librement diffusable.","/library/knowledge/modules/wikipedia-zim/wikipedia_fr_all_09_2012.zim"); ?>
-<?php hard_link("/iiab/zim/iframe/wikipedia_ar_all_02_2013", "ويكيبيديا", "ويكيبيديا كاملة باللغة العربية","/library/knowledge/modules/wikipedia-zim/wikipedia_ar_all_02_2013.zim"); ?>
-<?php hard_link("/iiab/zim/iframe/wikipedia_sw_all_04_2011", "Wikipedia katika Swahili", "Wikipedia ni kamusi elezo huru ya lugha nyingi katika mtandao.","/library/knowledge/modules/wikipedia-zim/wikipedia_sw_all_04_2011.zim"); ?>
 
+<a href="http://schoolserver:3000/wikipedia_rw_all_2015_04">Wikipedia in Kinyarwanda</a>
+<!--
+<?php hard_link("/iiab/zim/iframe/wikipedia_sw_all_04_2011", "Wikipedia katika Swahili", "Wikipedia ni kamusi elezo huru ya lugha nyingi katika mtandao.","/library/knowledge/modules/wikipedia-zim/wikipedia_sw_all_04_2011.zim"); ?>
+-->
 <h2>Maps</h2>
 <?php hard_link("/iiab/static/map.html", "Open Street Maps", "Open Street Maps from Internet-in-a-Box","/library/knowledge/modules/openstreetmap/mod_tile64"); ?>
 
