d1b568a89b97e825410b6850e9ea449e510b134b
Tim Moody <tim@timmoody.com>
2015-04-14 10:39:45 -0400

    more items

--- root/var/www/html/xs-portal/index.en.php
+++ root/var/www/html/xs-portal/index.en.php
@@ -25,12 +25,12 @@
 	<div class="xsServiceLink"><a href="http://schoolserver:3000" target="_blank">KIWIX</a></div>
   <div class="xsServiceDesc">KIWIX is another collection of Wikipedias, Wikidictionaries, Project Gutenberg and other Internet content served offline.</div>
 </div>
-<!--
+
 <div class="xsServiceWrapper">
 	<div class="xsServiceLink"><a href="http://schoolserver:3000/wikipedia_rw_all_2015-04" target="_blank">Kinyarwanda Wikipedia</a></div>
-  <div class="xsServiceDesc">KIWIX is another collection of Wikipedias, Wikidictionaries, Project Gutenberg and other Internet content served offline.</div>
+  <div class="xsServiceDesc">The Kinyarwanda Wikipedia is on the KIWIX menu.</div>
 </div>
--->
+
 <h2>Khan Academy</h2>
 
 <div class="xsServiceWrapper">
@@ -45,8 +45,8 @@
 
 <h2>More Services</h2>
 <div class="xsServiceWrapper">
-	<div class="xsServiceLink"><a href="http://schoolserver/owncloud" target="_blank">Own Cloud</a></div>
-  <div class="xsServiceDesc">Owncloud is an offline file sharing, calendaring, and collaboration server.</div>
+	<div class="xsServiceLink"><a href="http://schoolserver/owncloud" target="_blank">Your Own Cloud</a></div>
+  <div class="xsServiceDesc">owncloud is an offline file sharing, calendaring, and collaboration server.</div>
 </div>
 
 <div class="xsServiceWrapper">
@@ -55,6 +55,22 @@
 </div>
 
 <?php hard_link("/content", "Access Other Content", "Put additional content in /library/content and any subdirectories and link to it with this function, such as syans in Haiti."); ?>
+
+<h2>Tools for Administrators</h2>
+<div class="xsServiceWrapper">
+	<div class="xsServiceLink"><a href="http://schoolserver:5984/xovis/_design/xovis-couchapp/index.html" target="_blank">XOVIS</a></div>
+  <div class="xsServiceDesc">XOVIS is an volunteer contributed software to graph statistics on XO use of Activities.</div>
+</div>
+
+<div class="xsServiceWrapper">
+	<div class="xsServiceLink"><a href="http://schoolserver:5984/xovis/_design/xovis-couchapp/index.html" target="_blank">MUNIN</a></div>
+  <div class="xsServiceDesc">MUNIN is an administrative application to monitor server performance (user admin, password munindxs).</div>
+</div>
+
+<div class="xsServiceWrapper">
+	<div class="xsServiceLink"><a href="http://schoolserver/admin" target="_blank">Admin Console</a></div>
+  <div class="xsServiceDesc">The Admin Console permits control of the server using a graphical user interface (user xsce-admin, password g0adm1n).</div>
+</div>
 <!--
 <h2>Other Content</h2>
 <?php hard_link("/content", "Browse Other Content", "This is the place for local downloads."); ?>
